7c3eccc382a723c162ee3a19137952d6c8a37c83
ygrenzinger <yannick.grenzinger@gmail.com>
2015-10-20 17:04:22 +0200

    Bowling Game Test

---
+++ src/main/java/BowlingGame/BowlingGame.java
@@ -0,0 +1,47 @@
+package BowlingGame;
+
+import java.util.stream.IntStream;
+
+public class BowlingGame {
+
+    public static final char MISS = '-';
+    public static final char SPARE = '/';
+    public static final char STRIKE = 'X';
+
+    public static int scoreFor(String rolls) {
+        return IntStream.range(0, rolls.length()).map(i -> scoreOfRoll(rolls, i)).sum();
+    }
+
+    private static int scoreOfRoll(String rolls, int i) {
+        switch (rolls.charAt(i)) {
+            case MISS:
+                return valueOf(rolls.charAt(i));
+            case SPARE:
+                return valueOf(rolls.charAt(i)) + spare(rolls, i);
+            case STRIKE:
+                return valueOf(rolls.charAt(i)) + bonusStrikeRoll(rolls, i);
+            default: return valueOf(rolls.charAt(i));
+        }
+    }
+
+    private static int valueOf(Character character) {
+        if (character == MISS) return 0;
+        if (character == STRIKE || character == SPARE) return 10;
+        return Integer.valueOf(String.valueOf(character));
+    }
+
+    private static int bonusStrikeRoll(String rolls, int i) {
+        if (i >= rolls.length() - 3) return 0; //if we are on bonus roll of last (tenth) frame
+        return valueOf(rolls.charAt(i + 1)) + valueOf(rolls.charAt(i + 2));
+    }
+
+    private static int spare(String rolls, int i) {
+        return -scoreOfRoll(rolls, i - 1) + bonusSpareRoll(rolls, i);
+    }
+
+    private static int bonusSpareRoll(String rolls, int i) {
+        if (i == rolls.length() - 2) return 0; //if we are on bonus roll of last (tenth) frame
+        return valueOf(rolls.charAt(i + 1));
+    }
+
+}
---
+++ src/test/java/BowlingGame/BowlingGameTest.java
@@ -0,0 +1,49 @@
+package BowlingGame;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+public class BowlingGameTest {
+
+    @Test
+    public void should_have_a_score_of_10() {
+        assertThat(BowlingGame.scoreFor("11111111111111111111")).isEqualTo(20);
+    }
+
+    @Test
+    public void should_have_a_score_of_50() {
+        assertThat(BowlingGame.scoreFor("5-5-5-5-5-5-5-5-5-5-")).isEqualTo(50);
+    }
+
+    @Test
+    public void should_have_a_score_of_55() {
+        assertThat(BowlingGame.scoreFor("5/5-5-5-5-5-5-5-5---")).isEqualTo(55);
+    }
+
+    @Test
+    public void should_have_a_score_of_45() {
+        assertThat(BowlingGame.scoreFor("5-5-5-5/-5-5-5-5---")).isEqualTo(45);
+    }
+
+    @Test
+    public void should_have_a_score_of_60() {
+        assertThat(BowlingGame.scoreFor("5-5-5-5-5-5-5-5-5-5/5")).isEqualTo(60);
+    }
+
+    @Test
+    public void should_have_a_score_of_150() {
+        assertThat(BowlingGame.scoreFor("5/5/5/5/5/5/5/5/5/5/5")).isEqualTo(150);
+    }
+
+    @Test
+    public void should_have_a_score_of_240() {
+        assertThat(BowlingGame.scoreFor("XXXXXXXXX--")).isEqualTo(7 * 30 + 20 + 10);
+    }
+
+    @Test
+    public void should_have_a_score_of_300() {
+        assertThat(BowlingGame.scoreFor("XXXXXXXXXXXX")).isEqualTo(300);
+    }
+
+}
