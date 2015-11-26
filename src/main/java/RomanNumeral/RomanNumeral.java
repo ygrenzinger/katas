bba1279da2ac00919d4c42297c018372191f8c1d
ygrenzinger <yannick.grenzinger@gmail.com>
2015-11-26 16:27:33 +0100

    from Arabic to Roman Numeral

--- src/main/java/RomanNumeral/RomanNumeral.java
+++ src/main/java/RomanNumeral/RomanNumeral.java
@@ -1,21 +1,39 @@
 package RomanNumeral;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
 import java.util.ArrayDeque;
+import java.util.List;
 import java.util.Queue;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public enum RomanNumeral {
     I(1, 'I'), V(5, 'V'), X(10, 'X'), L(50, 'L'), C(100, 'C'), D(500, 'D'), M(1000, 'M');
 
+    public static RomanNumeral[] units = {C, X, I};
+
     private int decimalValue;
-    private int character;
+    private char character;
 
     RomanNumeral(int value, char character) {
         decimalValue = value;
         this.character = character;
     }
 
-    public static int stringToDecimal(String input) {
-        if (input.length() == 1)  {
+    public String toString() {
+        return String.valueOf(character);
+    }
+
+    private static List<RomanNumeral> romanNumeralsByDescendingOrder = Stream.of(RomanNumeral.values())
+            .sorted((o1, o2) -> o2.decimalValue - o1.decimalValue)
+            .collect(Collectors.toList());
+
+    public static int romanToArabic(String input) {
+        if (input.length() == 1) {
             return findCorrespondingNumeral(input.charAt(0)).decimalValue;
         }
         Queue<RomanNumeral> numerals = new ArrayDeque<>();
@@ -34,7 +52,7 @@ public enum RomanNumeral {
     private static int retrieveNextDecimal(Queue<RomanNumeral> numerals) {
         RomanNumeral current = numerals.remove();
         RomanNumeral next = numerals.peek();
-        if (next != null){
+        if (next != null) {
             switch (current) {
                 case I:
                     switch (next) {
@@ -80,4 +98,60 @@ public enum RomanNumeral {
         }
         throw new IllegalArgumentException(character + " is not a roman litteral");
     }
+
+    public static String arabicToRoman(final int arabicNumber) {
+        List<Integer> remainingValue = Stream.of(units).map(unit -> unit.decimalValue)
+                .map(decimalValue -> arabicNumber % decimalValue)
+                .collect(Collectors.toList());
+        return IntStream.range(0, units.length).mapToObj(i -> buildRomanNumberForUnit(remainingValue.get(i), units[i])).collect(Collectors.joining());
+
+        /*
+        for (RomanNumeral romanDecimalUnit : units) {
+            romanNumber += buildRomanNumberForUnit(arabicNumber, romanDecimalUnit);
+            arabicNumber = arabicNumber % romanDecimalUnit.decimalValue;
+        }
+        return romanNumber;
+        */
+    }
+
+    private static String buildRomanNumberForUnit(int arabicNumber, RomanNumeral romanDecimalUnit) {
+        final int unitValue = arabicNumber / romanDecimalUnit.decimalValue;
+        switch (romanDecimalUnit) {
+            case C:
+                if (unitValue > 0) {
+                    return findRomanNumberForCurrentUnit(unitValue, M, D, C);
+                }
+                break;
+            case X:
+                if (unitValue > 0) {
+                    return findRomanNumberForCurrentUnit(unitValue, C, L, X);
+                }
+                break;
+            default:
+                return findRomanNumberForCurrentUnit(unitValue, X, V, I);
+        }
+        return "";
+    }
+
+    private static String findRomanNumberForCurrentUnit(int unitValue, RomanNumeral upperUnit, RomanNumeral middleUnit, RomanNumeral lowerUnit) {
+        if (unitValue == 4) {
+            return lowerUnit.toString() + middleUnit.toString();
+        }
+        if (unitValue == 9) {
+            return lowerUnit.toString() + upperUnit.toString();
+        }
+        String romanNumbers = "";
+        if (unitValue >= 5) {
+            romanNumbers += middleUnit.toString();
+            unitValue -= middleUnit.decimalValue;
+        }
+        if (unitValue > 0 && unitValue < 4) {
+            romanNumbers += IntStream.range(0, unitValue)
+                    .mapToObj(i -> lowerUnit.toString())
+                    .collect(Collectors.joining());
+        }
+        return romanNumbers;
+    }
+
+
 }
--- src/test/java/RomanNumeral/RomanNumeralTest.java
+++ src/test/java/RomanNumeral/RomanNumeralTest.java
@@ -1,6 +1,5 @@
 package RomanNumeral;
 
-import RomanNumeral.RomanNumeral;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,51 +7,94 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RomanNumeralTest {
 
     @Test
-    public void equal1() {
-        assertThat(RomanNumeral.stringToDecimal("I")).isEqualTo(1);
+    public void should_return_I_when_1() {
+        assertThat(RomanNumeral.arabicToRoman(1)).isEqualTo("I");
     }
 
     @Test
-    public void equal4() {
-        assertThat(RomanNumeral.stringToDecimal("IV")).isEqualTo(4);
+    public void should_return_II_when_2() {
+        assertThat(RomanNumeral.arabicToRoman(2)).isEqualTo("II");
     }
 
     @Test
-    public void equal5() {
-        assertThat(RomanNumeral.stringToDecimal("V")).isEqualTo(5);
+    public void should_return_III_when_3() {
+        assertThat(RomanNumeral.arabicToRoman(3)).isEqualTo("III");
     }
 
     @Test
-    public void equal6() {
-        assertThat(RomanNumeral.stringToDecimal("VI")).isEqualTo(6);
+    public void should_return_IV_when_4() {
+        assertThat(RomanNumeral.arabicToRoman(4)).isEqualTo("IV");
     }
 
     @Test
-    public void equal9() {
-        assertThat(RomanNumeral.stringToDecimal("IX")).isEqualTo(9);
+    public void should_return_V_when_5() {
+        assertThat(RomanNumeral.arabicToRoman(5)).isEqualTo("V");
     }
 
     @Test
-    public void equal11() {
-        assertThat(RomanNumeral.stringToDecimal("XI")).isEqualTo(11);
+    public void should_return_VII_when_7() {
+        assertThat(RomanNumeral.arabicToRoman(7)).isEqualTo("VII");
     }
 
     @Test
-    public void equal1904() {
-        assertThat(RomanNumeral.stringToDecimal("MCMIV")).isEqualTo(1904);
+    public void should_return_IX_when_9() {
+        assertThat(RomanNumeral.arabicToRoman(9)).isEqualTo("IX");
     }
 
     @Test
-    public void equal1954() {
-        assertThat(RomanNumeral.stringToDecimal("MCMLIV")).isEqualTo(1954);
+    public void should_return_XXVII_when_27() {
+        assertThat(RomanNumeral.arabicToRoman(27)).isEqualTo("XXVII");
     }
 
     @Test
-    public void equal1990() {
-        assertThat(RomanNumeral.stringToDecimal("MCMXC")).isEqualTo(1990);
+    public void should_return_CMXCIX_when_999() {
+        assertThat(RomanNumeral.arabicToRoman(999)).isEqualTo("CMXCIX");
     }
 
+    @Test
+    public void should_return_1_when_I() {
+        assertThat(RomanNumeral.romanToArabic("I")).isEqualTo(1);
+    }
 
+    @Test
+    public void should_return_4_when_IV() {
+        assertThat(RomanNumeral.romanToArabic("IV")).isEqualTo(4);
+    }
+
+    @Test
+    public void should_return_5_when_V() {
+        assertThat(RomanNumeral.romanToArabic("V")).isEqualTo(5);
+    }
+
+    @Test
+    public void should_return_6_when_VI() {
+        assertThat(RomanNumeral.romanToArabic("VI")).isEqualTo(6);
+    }
+
+    @Test
+    public void should_return_9_when_IX() {
+        assertThat(RomanNumeral.romanToArabic("IX")).isEqualTo(9);
+    }
+
+    @Test
+    public void should_return_11_when_XI() {
+        assertThat(RomanNumeral.romanToArabic("XI")).isEqualTo(11);
+    }
+
+    @Test
+    public void should_return_1904_when_MCMIV() {
+        assertThat(RomanNumeral.romanToArabic("MCMIV")).isEqualTo(1904);
+    }
+
+    @Test
+    public void should_return_1954_when_MCMLIV() {
+        assertThat(RomanNumeral.romanToArabic("MCMLIV")).isEqualTo(1954);
+    }
+
+    @Test
+    public void should_return_1990_when_MCMXC() {
+        assertThat(RomanNumeral.romanToArabic("MCMXC")).isEqualTo(1990);
+    }
 
 
 }
